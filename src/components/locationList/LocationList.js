2c7309d02f562b523f3af31ca8d569df1654a69c
justinavickers <justinav38@gmail.com>
2018-11-16 11:44:34 -0600

    created first react app

---
+++ src/components/locationList/LocationList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class LocationList  extends Component {
+  render() {
+      return (
+          <article>
+              <h1>Location List</h1>
+              <section>Bellevue</section>
+              <section>Green Hills</section>
+              <section>Franklin</section>
+              <section>East Nashville</section>
+          </article>
+      );
+  }
+}
